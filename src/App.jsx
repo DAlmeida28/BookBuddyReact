a3cdeb1da783b0ffa804dd3be73c24aefe9a1d69
Daniel Almeida <almeida.danny@gmail.com>
2025-02-12 15:51:50 -0600

    updated login features and auth page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import {Routes, Route} from 'react-router-dom'
 import {useState} from 'react'  
 
-import BookList from './BookList.jsx'
+import BookList from './BookList.jsx';
 import BookDetails from './BookDetails.jsx';
+import Navbar from './Navbar.jsx';
+import Auth from './Auth.jsx'
 
 function App() {
   const [bookDetails, setBookDetails] = useState('');
+  const [token, setToken] = useState('');
 
   return (
     <>
+    <Navbar />
 
     <Routes>
       <Route path="/" element={<BookList setBookDetails={setBookDetails}/>}/>
       <Route path="/bookdetails/:bookid" element={<BookDetails bookDetails={bookDetails} setBookDetails={setBookDetails}/>}/>
+      <Route path="/auth" element={<Auth setToken={setToken} />}/>
     </Routes>
     </>
   )
---
+++ src/Auth.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Auth = (setToken) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [emailRegister, setEmailRegister] = useState('');
+  const [passwordRegister, setPasswordRegister] = useState('');
+
+  const [emailLogin, setEmailLogin] = useState('');
+  const [passwordLogin, setPasswordLogin] = useState('');
+  const [showLogin, setShowLogin] = useState(true);
+  const navigate = useNavigate();
+
+
+  const submitRegister = async (event) => {
+    event.preventDefault();
+
+    const reponse = await fetch ('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/register',{ 
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: firstName,
+        lastname: lastName,
+        email: emailRegister,
+        password: passwordRegister
+      })
+    }
+  )
+const response = await reponse.json();
+// console.log(response)
+  }
+
+const loginRequest = async (event) => {
+  event.preventDefault();
+
+  const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: emailLogin,
+      password: passwordLogin
+    })
+  })
+  const reply = await response.json();
+  console.log(reply)
+  setToken(reply.token);  
+}
+
+  return (
+    <>
+
+    {showLogin ?
+    <>
+      <input onChange={((event) => {setEmailLogin(event.target.value)})}type="email" placeholder="email"/>
+      <input onChange={((event) => {setPasswordLogin(event.target.value)})}type="password" placeholder="password"/>
+      <button onClick={loginRequest}>Login</button>
+      <button onClick={() => {setShowLogin(false)}}> Register </button> 
+      <button onClick={() => {navigate('/')}}>Back to Book List</button>
+    </>     
+      :
+      <section>
+        <form onSubmit={submitRegister}>
+          <input onChange={(event) => {setFirstName(event.target.value)}}placeholder="firstname"/>
+          <input onChange={(event) => {setLastName(event.target.value)}}placeholder="lastname"/>
+          <input onChange={(event) => {setEmailRegister(event.target.value)}}type="email" placeholder="email"/>
+          <input onChange={(event) => {setPasswordRegister(event.target.value)}}type="password" placeholder="password"/>
+          <button> Submit </button>
+        </form>
+      </section>
+}
+    </>
+  )
+}
+
+export default Auth
