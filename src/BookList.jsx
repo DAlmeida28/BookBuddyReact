246f45ae92a7d99e027f9aac111801ce414e3766
Daniel Almeida <almeida.danny@gmail.com>
2025-02-12 11:26:30 -0600

    add book details pages

--- src/BookList.jsx
+++ src/BookList.jsx
@@ -1,32 +1,39 @@
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom";
 
-const BookList = () => {
-  const[allBooks, setAllBooks] = useState([]);
+const BookList = ({ setBookDetails }) => {
+  const [allBooks, setAllBooks] = useState([]);
+  const navigate = useNavigate();
 
-useEffect(() => {
-  const getBooks = async () => {
-    const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books');
-    const booksArray = await response.json();
-    setAllBooks(booksArray.books);
-  }
-  getBooks();
-},[])
+  useEffect(() => {
+    const getBooks = async () => {
+      const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books');
+      const booksArray = await response.json();
+      setAllBooks(booksArray.books);
+    }
+    getBooks();
+  }, [])
 
-return(
-  <>
-    <h1>List of Our Books: </h1>
+  return (
+    <>
+      <h1>List of Our Books: </h1>
 
-  <section id="BookList">
-  {allBooks.map((book) => {
-    
-    return(
-      <p key={book.id}>
-        {book.title}
-        <img src={book.coverimage} height="450px" width="200px"/>
-        </p>
-    )
-  })}
-    </section>
+      <section id="BookList">
+        {allBooks.map((book) => {
+
+          return (
+            <p key={book.id}
+              onClick={() => {
+                setBookDetails(book);
+                navigate(`/bookdetails/${book.id}`);
+              }
+              }>
+              {book.title}
+              <img src={book.coverimage} height="450px" width="200px" />
+            </p>
+          )
+        })}
+      </section>
     </>
   )
 }
